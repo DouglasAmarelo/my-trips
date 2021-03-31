a09771f02d2cad7770bfd03f3dc9b95e0bae17a3
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2021-03-31 09:05:56 -0300

    add component Map and his test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
-import Main from '@components/Main';
+import dynamic from 'next/dynamic';
+
+const Map = dynamic(() => import('@components/Map'), { ssr: false });
 
 const Home = () => {
-  return <Main />;
+  return (
+    <Map
+      places={[
+        {
+          id: '1',
+          name: 'Petrópolis',
+          slug: 'petropolis',
+          location: {
+            latitude: 35,
+            longitude: -12,
+          },
+        },
+        {
+          id: '2',
+          name: 'Reykjavik',
+          slug: 'reykjavik',
+          location: {
+            latitude: 45,
+            longitude: -10,
+          },
+        },
+      ]}
+    />
+  );
 };
 
 export default Home;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eeeeee;
+    --container: 100rem;
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -12,11 +22,27 @@ const GlobalStyles = createGlobalStyle`
   }
 
   html, body, #__next {
+    background: var(--background);
+    color: var(--white);
     height: 100%;
   }
 
   body {
     font-family: ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+
+  p {
+    line-height: var(--medium);
+  }
+
+  p,
+  a {
+    font-size: 2rem;
+  }
+
+  a {
+    color: var(--highlight);
   }
 `;
 
